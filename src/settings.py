a96a217e57c1820e8d7de04ee34001876208c002
erastov-alex <erastov.alex@gmail.com>
2023-10-18 12:16:53 +0300

    set preview img

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/erickendric/tree-dataset-of-urban-street-classification-fruit"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 7022746
+PREVIEW_IMAGE_ID: int = 7370886
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/urban-street-fruit"
