88537af7f94c2aa001191180b5c7105a32c5f819
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-07 15:10:14 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -43,9 +43,7 @@ GITHUB_URL: str = "https://github.com/dataset-ninja/urban-street-fruit"
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = [
-    "https://www.kaggle.com/datasets/erickendric/tree-dataset-of-urban-street-classification-fruit"
-]
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://www.kaggle.com/datasets/erickendric/tree-dataset-of-urban-street-classification-fruit"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -72,6 +70,7 @@ AUTHORS: Optional[List[str]] = [
     "Junhua Ye",
     "Jianxin Yin",
 ]
+AUTHORS_CONTACTS: Optional[List[str]] = ["xuaj1976@163.com"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
     "Zhejiang Agriculture and Forestry University"
@@ -163,6 +162,7 @@ def get_settings():
     settings["repository"] = REPOSITORY
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
